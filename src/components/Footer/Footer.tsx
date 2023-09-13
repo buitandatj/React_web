976e87b4c152c71bcb26a451c781fa861447fde3
dat <dat@gmail.com>
2023-09-13 15:01:06 +0700

    create footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './style.scss';
+import { FiPhoneCall } from "react-icons/fi";
+import {FaFacebookSquare,FaInstagramSquare,FaTwitterSquare} from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <footer className='footer mt-5 bg-[#000000]'>
+            <div className='container-footer container mx-auto w-[1170px] flex flex-wrap py-20 justify-center  content-center'>
+                <div className='sm:w-1/1 md:w-1/2 lg:w-1/4'>
+                    <h4 className='text-[#9B9B9B] text-[18px] font-semibold'>Về DEGREY</h4>
+                    <p className='pt-5'>
+                        Cái tên Degrey được tạo ra rất ngẫu hứng, xuất phát từ “Chuỗi ngày u buồn về sự nghiệp,
+                        tương lai trong quá khứ của chính mình” theo lời chia sẻ của Degreys founder.
+                    </p>
+                    <div className='pt-4'>
+                        <h5 className='font-semibold'>CÔNG TY TNHH DEGREY</h5>
+                        <p>
+                            0315986019 0315986019 cấp ngày 31/10/2019 tại Sở kế hoạch đầu tư TPHCM
+                        </p>
+                    </div>
+                    <a href='http://online.gov.vn/Home/WebDetails/104783' target="_blank" className='mt-4 block'>
+                        <img className='w-[150px] h-[57px]' src="https://file.hstatic.net/1000281824/file/dathongbao_22c990fc936b40e0a50a1b646f478284.png" alt="" />
+                    </a>
+                </div>
+                <div className='sm:w-1/1 md:w-1/2 lg:w-1/4 pl-5 pt-12'>
+                    <p className='font-bold'>Địa chỉ</p>
+                    <p className='font-bold pt-7'>Sài Gòn:</p>
+                    <p>
+                        43 Huỳnh Văn Bánh P.17 Q.Phú Nhuận
+                        1041 Luỹ Bán Bích P.Tân Thành Q.Tân Phú
+                    </p>
+                    <p className='pt-4'>
+                        <span className='font-bold'>Điện thoại:</span> 0336311117
+                    </p>
+                    <p  className='pt-4'>
+                        <span className='font-bold'>Email:</span> degrey.vn@gmail.com
+                    </p>
+                </div>
+                <div className='sm:w-1/1 md:w-1/2 lg:w-1/4 pl-10'>
+                    <h4 className='text-[#9B9B9B] text-[18px] font-semibold'>Hỗ trợ khách hàng</h4>
+                    <ul className='pt-5 leading-8'>
+                        <li><a >Giới thiệu Degrey</a></li>
+                        <li><a >Hệ thống cửa hàng</a></li>
+                        <li><a >Hướng dẫn đặt hàng</a></li>
+                        <li><a >Chính sách và quy định</a></li>
+                        <li><a >Chính sách bảo mật</a></li>
+                        <li><a href="">Thông tin sở hửu</a></li>
+                    </ul>
+                </div>
+                <div className='sm:w-1/1 sm:pl-10 md:w-1/2  lg:w-1/4'>
+                    <h4 className='text-[#9B9B9B] text-[18px] font-semibold'>Chăm sóc khách hàng</h4>
+                    <div className='flex flex-wrap pt-5'>
+                        <FiPhoneCall className=' h-[30px] w-[30px] text-[#9B9B9B] mt-4' />
+                        <p className='text-[#9B9B9B] text-[26px] font-semibold pl-5'>0336311117</p>
+                        <p className='pl-11 underline'>degrey.vn@gmail.com</p>
+                    </div>
+                    <div>
+                        <p  className='text-[#9B9B9B] text-[18px] font-semibold pt-7'>Follow Us</p>
+                    </div>
+                    <div className='flex gap-3 mt-5 cursor-pointer' >
+                        <FaFacebookSquare className='bg-white text-[30px]'/>
+                        <FaInstagramSquare className='bg-white text-[30px]'/>
+                        <FaTwitterSquare className='bg-white text-[30px]'/>
+                    </div>
+                </div>
+            </div>
+            <p className='justify-center flex text-[#9B9B9B]'>Copyright 2023 DEGREY VIETNAM. by BUITANDAT</p>
+        </footer>
+    );
+};
+
+export default Footer;
