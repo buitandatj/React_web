eae56ec2153a11856a8e4ca08cefe6fc2083e693
dat <dat@gmail.com>
2023-09-18 14:39:55 +0700

    create folder constants

---
+++ src/constants/Constants.ts
@@ -0,0 +1,6 @@
+export const video ="https://www.youtube.com/embed/kQ_pyG0jatk";
+export const map ="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6598.932228303694!2d106.67885742862045!3d10.794591218186346!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528d411c35277%3A0xcf96cde06627a938!2zRGVncmV5LnZuIC0gU8OgaSBHw7JuIC0gNDMgSHXhu7NuaCBWxINuIELDoW5oIFAuMTcgUS5QaMO6IE5odeG6rW4!5e0!3m2!1svi!2s!4v1694666208978!5m2!1svi!2s"
+export const facebook ="https://www.facebook.com/degrey.saigon"
+export const insta ="https://www.instagram.com/degrey.saigon/"
+export const tiktok = "https://www.tiktok.com/@degrey.vn"
+export const shoppe = "https://shopee.vn/degrey.vn"
