392149a809b0168d4a915e8dbe825a21901838c1
dat <dat@gmail.com>
2023-09-22 16:59:45 +0700

    update cartItem

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,33 +1,31 @@
-import React, { useContext } from 'react';
 import { FaDeleteLeft } from 'react-icons/fa6'
-import { IState } from '../../context/productContext';
 import { Link } from 'react-router-dom';
-import { CartContext } from '../../context/cartContext';
+import { IProducts } from '../../type/IProducts';
+import useCart from '../../helper/useCart';
 
 
 export const formatPrice = (price: number) => {
-    return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+    return price?.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
 };
-const CartItem = ({ item }: { item: IState }) => {
-    const { deleteItemCart, increaseMount, decreaseMount }: any = useContext(CartContext)
-    // const { id, title, image, mount, price }: IState[] = item
-
+const CartItem = ({ item, }: { item: IProducts }) => {
+    const { id, title, image, price, mount } = item
+    const { DeleteItemCart, Amount } = useCart()
     return (
         <div className='item-cart'>
             <div className='item-left'>
-                <img src={item.image} alt={item.title} />
-                <Link to={`/products/${item.id}`}><p>{item.title}</p></Link>
+                <img src={image} alt={title} />
+                <Link to={`/products/${id}`}><p>{title}</p></Link>
             </div>
             <div className='item-right'>
-
-                <p>{formatPrice(item.price * item.mount)}</p>
+                <p>{formatPrice(price * mount)}</p>
+                
                 <div className='right-child'>
-                    <div onClick={() => decreaseMount(item.id)}><button>-</button></div>
+                    <div onClick={() => { Amount(id, false) }}><button>-</button></div>
                     <div>{item.mount}</div>
-                    <div onClick={() => increaseMount(item.id)}><button>+</button></div>
+                    <div onClick={() => { Amount(id, true) }}><button>+</button></div>
                 </div>
             </div>
-            <button className='delete-item' onClick={() => deleteItemCart(item.id)}><FaDeleteLeft /></button>
+            <button className='delete-item' onClick={() => DeleteItemCart(id)}><FaDeleteLeft /></button>
         </div>
     );
 };
