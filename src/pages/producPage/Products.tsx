6953cad8f818aa98dd6ad7268486751f49db695d
dat <dat@gmail.com>
2023-09-22 16:58:39 +0700

    update pageProducts

--- src/pages/producPage/Products.tsx
+++ src/pages/producPage/Products.tsx
@@ -1,40 +1,54 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useMemo } from 'react';
 import Banner from '../../components/Banner/Banner';
 import './style.scss'
 import Filter from '../../components/Filter/Filter';
 import Product from '../../components/Product/Product';
-import { IState, ProductContext } from '../../context/productContext';
+import { ProductContext } from '../../context/productContext';
 import ReactPaginate from 'react-paginate';
+import { useSearchParams } from 'react-router-dom';
+import { IProducts } from '../../type/IProducts';
 
-
+const productsPage = 10;
 const Products = () => {
-    const { products }: { products: IState[] } = useContext(ProductContext)
-    const [currentPage, setCurrentPage] = useState(0);
-    const productsPage = 10;
-    const pages = currentPage * productsPage;
-
-    const handlePageChange = ({ selected }: { selected: number }) => {
-        setCurrentPage(selected);
+    useEffect(() => {
         window.scrollTo(0, 0)
+    }, [])
+    const { products }: { products: IProducts[] } = useContext(ProductContext)
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get('q')
+    const currentPage = searchParams.get('p') ? parseInt(searchParams.get('p') as string) : 1
+    const pages = currentPage * productsPage;
+    const pageCount = Math.ceil(products.length / productsPage);
+    const handlePageChange = ({ selected }: { selected: number }) => {
+        searchParams.set("p", selected + "")
+        setSearchParams(searchParams)
+        window.scrollTo(0, 450)
     };
+    const data = useMemo(() => {
+        let result = products;
+        if (query) {
+            result = products.filter((item) => item.title.toLowerCase().includes(query.toLowerCase()))
+        }
+        result = result.slice(pages, pages + productsPage)
+        return result
+    }, [products, query, pages])
+
     return (
         <div>
-
             <Banner />
             <Filter />
             <div className='products'>
-                {products?.slice(pages, pages + productsPage)
-                    .map((product: IState) => {
-                        return <Product key={product.id} product={product} />;
-                    })}
+                {data.map((product: IProducts) => {
+                    return <Product key={product.id} product={product} />;
+                })}
             </div>
             <ReactPaginate
                 nextLabel=">"
                 previousLabel="<"
                 onPageChange={handlePageChange}
-                pageRangeDisplayed={3}
-                marginPagesDisplayed={2}
-                pageCount={3}
+                initialPage={currentPage}
+                pageRangeDisplayed={pageCount}
+                pageCount={pageCount}
                 pageClassName="page-item"
                 pageLinkClassName="page-link"
                 previousClassName="page-item"
