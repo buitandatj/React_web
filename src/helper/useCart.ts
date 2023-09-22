baf17ef9a846962e517992325cabce1cfb9dacec
dat <dat@gmail.com>
2023-09-22 16:57:12 +0700

    create helper

---
+++ src/helper/useCart.ts
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../context/cartContext';
+import { IProducts } from '../type/IProducts';
+import { addCart } from '../constants/Message';
+
+const useCart = () => {
+  const { cart, setCart } = useContext(CartContext);
+
+  const AddToCart = (product: IProducts) => {
+    const newProduct = { ...product, mount: 1 };
+    const cartItem = cart.find((item) => {
+      return item.id === product.id;
+    });
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === product.id) {
+          return { ...item, mount: cartItem.mount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newProduct]);
+    }
+    addCart();
+  };
+  const DeleteItemCart = (id: number) => {
+    const newCart: IProducts[] = cart.filter((item) => {
+      return item.id !== id;
+    });
+    setCart(newCart);
+  };
+  const Amount = (id: number, isMount: boolean) => {
+    const newItemCart = cart.map((item) => {
+      if (item.id === id) {
+        if (isMount) {
+          return { ...item, mount: item.mount + 1 };
+        } else {
+          if (item.mount === 1) {
+            return item;
+          }
+          return { ...item, mount: item.mount - 1 };
+        }
+      }
+      return item;
+    });
+
+    setCart(newItemCart);
+  };
+  return { AddToCart, DeleteItemCart, Amount };
+};
+
+export default useCart;
