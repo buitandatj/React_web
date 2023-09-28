bc0965dce9b2f16b64d3872424b7123762e4041a
dat <dat@gmail.com>
2023-09-28 10:45:45 +0700

    create loadingContext

---
+++ src/context/loadingContext.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from 'react';
+
+
+export interface LoadingContextType {
+    loading: boolean;
+    setLoading: (value: boolean) => void;
+}
+type ChildrenProps = { children: JSX.Element };
+export const loadingContext = createContext<LoadingContextType>({ loading: false, setLoading: (value: boolean) => { } })
+
+export const LoadingProvider = ({ children }: ChildrenProps) => {
+
+    const [loading, setLoading] = useState<boolean>(false)
+
+    return (
+        <loadingContext.Provider value={{ loading, setLoading }}>
+            {children}
+        </loadingContext.Provider>
+    )
+}   
